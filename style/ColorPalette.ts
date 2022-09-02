ddb3d39328635d8d6729c49da6c62d77c957f937
Ari Wisenburn (RIT Student) <aew1756@banjo.rit.edu>
2022-09-02 08:44:45 -0400

    initial commit

---
+++ style/ColorPalette.ts
@@ -0,0 +1,7 @@
+let ROSE = 'f1dedc';
+let ENGLISH_VIOLET = '44355b';
+let OLIVINE = '9ab87a';
+let HUNTER_GREEN = '3f6634';
+let LINCOLN_GREEN = '345511';
+
+export * as ColorPalette from './ColorPalette';
